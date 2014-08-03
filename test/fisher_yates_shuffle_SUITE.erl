a4d12a812ce304a6b20159e8b482406f4b3ba33d
Stratus3D <admin@stratus3d.com>
2014-08-02 22:51:01 -0400

    Updated tests.

--- test/fisher_yates_shuffle_SUITE.erl
+++ test/fisher_yates_shuffle_SUITE.erl
@@ -10,11 +10,13 @@ test_shuffle(_Config) ->
     List = [1,2,3,4,5,6,7,8,9],
 
     % should return shuffled results
-    Result = fisher_yates_shuffle:shuffle(List),
+    List /= fisher_yates_shuffle:shuffle(List),
 
     % shuffle results should be different each time
     Result1 = fisher_yates_shuffle:shuffle(List),
     Result2 = fisher_yates_shuffle:shuffle(List),
+    Result1 /= List,
+    Result1 /= Result2,
 
     % shuffle should return all the elements
     9 = length(fisher_yates_shuffle:shuffle(List)),
@@ -23,5 +25,6 @@ test_shuffle(_Config) ->
     [1] = fisher_yates_shuffle:shuffle([1]),
 
     % should work when list contains duplicate items
+    3 = length(fisher_yates_shuffle:shuffle([1,2,1])),
     [1,1,1] = fisher_yates_shuffle:shuffle([1,1,1]).
 

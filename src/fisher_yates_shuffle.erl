2310b7e3a15d255f7e726b63fab059dfd29db0ba
Stratus3D <admin@stratus3d.com>
2014-08-02 22:26:21 -0400

    Fixed odd bug.

--- src/fisher_yates_shuffle.erl
+++ src/fisher_yates_shuffle.erl
@@ -9,16 +9,16 @@ shuffle(List) ->
     do_shuffle(List, []).
 
 
--spec do_shuffle(List :: list(), ResultList :: list()) -> list().
+-spec do_shuffle(List :: list(), ShuffledList :: list()) -> list().
 
 do_shuffle([], []) ->
     % When the list is empty we simply return an empty list
     [];
-do_shuffle([Item], ResultList) ->
+do_shuffle([Item], ShuffledList) ->
     % When we only have one item remaining we add it to the result
     % list and return the completed list.
-    [Item|ResultList];
-do_shuffle(List, ResultList) ->
+    [Item|ShuffledList];
+do_shuffle(List, ShuffledList) ->
     % Calculate length for range
     Len = length(List),
 
@@ -26,8 +26,7 @@ do_shuffle(List, ResultList) ->
     % index and remove it from the list of unshuffled items.
     Index = random:uniform(Len),
     Item = lists:nth(Index, List),
-    RemainingList = lists:delete(Index, List),
-    erlang:display(length(RemainingList)),
+    RemainingList = lists:delete(Item, List),
 
     % Process all remaining elements
-    do_shuffle(RemainingList, [Item|ResultList]).
+    do_shuffle(RemainingList, [Item|ShuffledList]).
--- test/fisher_yates_shuffle_SUITE.erl
+++ test/fisher_yates_shuffle_SUITE.erl
@@ -10,17 +10,18 @@ test_shuffle(_Config) ->
     List = [1,2,3,4,5,6,7,8,9],
 
     % should return shuffled results
-    fisher_yates_shuffle:shuffle([1,2,3,4,5]).
+    Result = fisher_yates_shuffle:shuffle(List),
 
     % shuffle results should be different each time
-    %Result1 = fisher_yates_shuffle:shuffle(List),
-    %Result2 = fisher_yates_shuffle:shuffle(List),
-    %
-    %% shuffle should return all the elements
-    %Result3 = fisher_yates_shuffle:shuffle(List),
-    %erlang:display(Result3),
-    %9 = length(Result3),
-    %
-    %% should work on lists with one item
-    %[1] = fisher_yates_shuffle:shuffle([1]).
+    Result1 = fisher_yates_shuffle:shuffle(List),
+    Result2 = fisher_yates_shuffle:shuffle(List),
+
+    % shuffle should return all the elements
+    9 = length(fisher_yates_shuffle:shuffle(List)),
+
+    % should work on lists with one item
+    [1] = fisher_yates_shuffle:shuffle([1]),
+
+    % should work when list contains duplicate items
+    [1,1,1] = fisher_yates_shuffle:shuffle([1,1,1]).
 
